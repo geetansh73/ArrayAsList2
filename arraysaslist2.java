e0bcf123fac309179aab7145e199e410fb1f0faa
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-01-21 19:08:47 +0530

    Add files via upload

---
+++ arraysaslist2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class arraysaslist2
+{
+public static void main(String g[])
+{
+String str[]={"a","b","c","d"};
+List <String>al=new ArrayList<>();
+for(String s:str)
+{
+al.add(s);
+}
+System.out.println(al);
+}
+}
